1a0e7e7ac1acabe332be88c0a2d6e029f7342d93
Terrence Crossdale <tcrossdale@gmail.com>
2021-07-27 19:33:48 -0400

    [TC] add react based site structure setup

---
+++ hbcu-tech-stories-react/build/precache-manifest.f0453c03fd72105adf3e74bcd8c595da.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a04f518fb40488887e03cfbbd655e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c80a0d52d1ccb254cbb5",
+    "url": "/static/css/main.22fb73d3.chunk.css"
+  },
+  {
+    "revision": "77713a3c50d2c81a6483",
+    "url": "/static/js/10.8a41b30d.chunk.js"
+  },
+  {
+    "revision": "6bbfe5558e728a5b2966",
+    "url": "/static/js/2.1c0cc182.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1c0cc182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b500eecb45cfe5f409f7",
+    "url": "/static/js/3.850dc6d6.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/static/js/3.850dc6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6273076005c39a3a6a6e",
+    "url": "/static/js/4.29543bf3.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/static/js/4.29543bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccfd434a94e8026e97a",
+    "url": "/static/js/5.5873a6bb.chunk.js"
+  },
+  {
+    "revision": "7e4d74eabcb47de83c94",
+    "url": "/static/js/6.4133236f.chunk.js"
+  },
+  {
+    "revision": "fba1c180b5253450784e",
+    "url": "/static/js/7.ca2a4240.chunk.js"
+  },
+  {
+    "revision": "d8c63632bf0f29b9ea36",
+    "url": "/static/js/8.42f98f25.chunk.js"
+  },
+  {
+    "revision": "a8fc9cf5baa6f976d1ff",
+    "url": "/static/js/9.d029c989.chunk.js"
+  },
+  {
+    "revision": "c80a0d52d1ccb254cbb5",
+    "url": "/static/js/main.5bd59f72.chunk.js"
+  },
+  {
+    "revision": "63227daed88fb68b17bc",
+    "url": "/static/js/runtime-main.f823b308.js"
+  }
+]);
