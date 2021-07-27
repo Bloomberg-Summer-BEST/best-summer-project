3d142c90ea9d090b75a717af0107c7fe7599505d
Terrence Crossdale <tcrossdale@gmail.com>
2021-07-27 19:38:52 -0400

    [TC] add react based site structure setup

--- src/views/pages/Community.js
+++
@@ -1,8 +0,0 @@
-
-import React, { useState } from "react";
-
-const Community = (props) => {
-    return (<h3>Community</h3>);
-};
-
-export default Community;
---
+++ static/js/8.42f98f25.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpSummer BEST Project"]=this["webpackJsonpSummer BEST Project"]||[]).push([[8],{505:function(e,t,n){"use strict";n.r(t);var r=n(3),u=n.n(r);t.default=function(){return u.a.createElement(u.a.Fragment,null)}}}]);
+//# sourceMappingURL=8.42f98f25.chunk.js.map
